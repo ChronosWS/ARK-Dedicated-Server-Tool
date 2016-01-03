28704542880d0c8e4792fbf146a22a3c16918498
Bletch <bletch1971@hotmail.com>
2016-01-03 16:44:04 +1000

    RCON Window Features
    1. BugFix - Added the owner to the commandline window, so when open and
    losses focus, the command line window is bought to the foreground.
    2. Change View Profile... menu option to View Steam Profile...
    3. Added new View Profile... menu item.
    4. Created a new Player Profile window - shows details about the
    selected player.
    5. Created a new Tribe Profile window - shows details about the selected
    player's tribe.
    
    NOTE: Both new windows are accessible via right-click on the player and
    selecting either View Profile or View Tribe.

---
+++ ARK Server Manager/PlayerProfile.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Navigation;
+using ARK_Server_Manager.Lib.ViewModel.RCON;
+using ArkData;
+
+namespace ARK_Server_Manager
+{
+    /// <summary>
+    /// Interaction logic for PlayerProfile.xaml
+    /// </summary>
+    public partial class PlayerProfile : Window
+    {
+        public PlayerProfile(PlayerInfo player)
+        {
+            InitializeComponent();
+
+            this.Player = player;
+            this.DataContext = this;
+        }
+
+        public PlayerInfo Player
+        {
+            get;
+            private set;
+        }
+
+        public Player ArkDataPlayer
+        {
+            get
+            {
+                return Player?.ArkData;
+            }
+        }
+
+        public Tribe ArkDataTribe
+        {
+            get
+            {
+                return Player?.ArkData?.Tribe;
+            }
+        }
+
+        public String CreatedDate
+        {
+            get
+            {
+                return ArkDataPlayer?.FileCreated.ToString("G");
+            }
+        }
+
+        public Boolean IsTribeOwner
+        {
+            get
+            {
+                return ArkDataPlayer != null && ArkDataTribe != null && ArkDataTribe.OwnerId == ArkDataPlayer.Id;
+            }
+        }
+
+        public String ProfileUrl
+        {
+            get
+            {
+                return ArkDataPlayer?.ProfileUrl;
+            }
+        }
+
+        public String TribeOwner
+        {
+            get
+            {
+                return ArkDataTribe != null && ArkDataTribe.Owner != null ? string.Format("{0} ({1})", ArkDataTribe.Owner.SteamName, ArkDataTribe.Owner.CharacterName) : null;
+            }
+        }
+
+        public String UpdatedDate
+        {
+            get
+            {
+                return ArkDataPlayer?.FileUpdated.ToString("G");
+            }
+        }
+
+        public String WindowTitle
+        {
+            get
+            {
+                return String.Format("Player Profile - {0}", Player.SteamName);
+            }
+        }
+
+        private void SteamName_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            if (sender.GetType() != typeof(Hyperlink)) return;
+
+            string link = ((Hyperlink)sender).NavigateUri.ToString();
+            if (String.IsNullOrWhiteSpace(link)) return;
+
+            Process.Start(link);
+            e.Handled = true;
+        }
+    }
+}
---
+++ ARK Server Manager/TribeProfile.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Navigation;
+using ARK_Server_Manager.Lib.ViewModel.RCON;
+using ArkData;
+
+namespace ARK_Server_Manager
+{
+    /// <summary>
+    /// Interaction logic for TribeProfile.xaml
+    /// </summary>
+    public partial class TribeProfile : Window
+    {
+        public TribeProfile(PlayerInfo player)
+        {
+            InitializeComponent();
+
+            this.Player = player;
+            this.DataContext = this;
+        }
+
+        public PlayerInfo Player
+        {
+            get;
+            private set;
+        }
+
+        public Player ArkDataPlayer
+        {
+            get
+            {
+                return Player?.ArkData;
+            }
+        }
+
+        public Tribe ArkDataTribe
+        {
+            get
+            {
+                return Player?.ArkData?.Tribe;
+            }
+        }
+
+        public String CreatedDate
+        {
+            get
+            {
+                return ArkDataTribe?.FileCreated.ToString("G");
+            }
+        }
+
+        public String TribeOwner
+        {
+            get
+            {
+                return ArkDataTribe != null && ArkDataTribe.Owner != null ? string.Format("{0} ({1})", ArkDataTribe.Owner.SteamName, ArkDataTribe.Owner.CharacterName) : null;
+            }
+        }
+
+        public String UpdatedDate
+        {
+            get
+            {
+                return ArkDataTribe?.FileUpdated.ToString("G");
+            }
+        }
+
+        public String WindowTitle
+        {
+            get
+            {
+                return String.Format("Tribe Profile - {0}", Player.TribeName);
+            }
+        }
+    }
+}
